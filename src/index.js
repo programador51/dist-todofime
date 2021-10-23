e7b0a2bf6d01faa6c013c353ccb117e35377ded5
programador51 <gskitcat@gmail.com>
2021-10-23 03:01:18 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require('module-alias/register');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', __dirname);
 const app_1 = require("./app");
@@ -9,4 +8,5 @@ if (process.env.NODE_ENV !== 'test') {
     const app = (0, app_1.createApp)();
     (0, server_1.startServer)(app);
 }
+require('module-alias/register');
 //# sourceMappingURL=index.js.map
