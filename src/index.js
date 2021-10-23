e8a530b71639297afb016912f11fd22b62a33a3b
programador51 <gskitcat@gmail.com>
2021-10-23 08:42:36 -0500

    feat: Servidor y BD

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require('module-alias/register');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', __dirname);
 const app_1 = require("./app");
@@ -8,5 +9,4 @@ if (process.env.NODE_ENV !== 'test') {
     const app = (0, app_1.createApp)();
     (0, server_1.startServer)(app);
 }
-require('module-alias/register');
 //# sourceMappingURL=index.js.map
